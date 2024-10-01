2beda15ef0dfe876ca19d08032572c4999ccfd36
sudhakarsoumya <22hr1a0519@mtieat.org>
2024-10-01 22:20:11 +0530

    Q4.py

---
+++ Q4.py
@@ -0,0 +1,39 @@
+...
+The rules for generating Collatz Sequence are:
+If n is even:   n = n / 2
+If n is odd:    n = 3n + 1
+For example, if the starting number is 5 the sequence is:
+5 -> 16 -> 8 -> 4 -> 2 -> 1
+It has been proved for almost all integers, the repeated application of the above rule will result in a sequence that ends at 1.
+Input format:
+The input containing an integer 'n' which denotes the given number
+Output format:
+Print the numbers in the sequence and also print the number of times the rule has to be applied in order to reach 1.
+Refer the sample output for formatting.
+Sample Input:
+5
+Sample Output:
+5
+16
+8
+4
+2
+1
+5
+....
+
+ans:
+def collatz_sequence(n):
+    count = 0
+    while n != 1:
+        print(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        count += 1
+    print(1)  # Printing the final 1 in the sequence
+    return count
+n = int(input())
+steps = collatz_sequence(n)
+print(steps)
